0fa3b60e6a7f3ea48adbae90c01ef39b3a6fe951
Charymyrat Garryyev <kcharymyrat@gmail.com>
2022-04-15 19:00:09 +0500

    third commit

--- cofee_machine_functional.py
+++ cofee_machine_functional.py
@@ -1,4 +1,21 @@
 # Stage 3
+"""
+Amount of ingredients per coffee
+
+esp_coffee = 16
+esp_water = 250
+esp_money = 4
+
+latte_coffee = 20
+latte_water = 350
+latte_milk = 75
+latte_money = 7
+
+cap_coffee = 12
+cap_water = 200
+cap_milk = 100
+cap_money = 6
+"""
 
 # To make 1 cup of coffee, we need:
 WATER = 400
@@ -31,123 +48,50 @@ def fill():
 
 def take():
     global WATER, MILK, COFFEE, CUP, MONEY
-
     MONEY = 0
 
 
-def buy_espresso():
+def buy_coffee(coffee, water, milk, money, coffee_type):
     global WATER, MILK, COFFEE, CUP, MONEY
-
-    esp_coffee = 16
-    esp_water = 250
-    esp_money = 4
 
     requested_cup_num = int(input("How many cups of coffee do you need: "))
 
-    max_cups_water = WATER // esp_water
-    # max_cups_milk = milk_stock // MILK
-    max_cups_coffee = COFFEE // esp_coffee
-    max_cups = min(max_cups_water, max_cups_coffee, CUP)
-
-    if max_cups >= requested_cup_num:
-        print("Yes, I can prepare that amount") if max_cups == requested_cup_num else print(
-            f"Yes, and {max_cups - requested_cup_num} more cup(s)")
-    else:
-        print(f"No, I can prepare only {max_cups} cup(s)") if max_cups != 0 else print(
-            "No, I can not prepare that amount")
-
-    if esp_coffee <= COFFEE and esp_water <= WATER:
-
-        WATER -= esp_water
-        COFFEE -= esp_coffee
-        CUP -= 1
-        MONEY += esp_money
-    else:
-        print("Sorry, but there is not enough ingredients")
-
-
-def buy_latte():
-    global WATER, MILK, COFFEE, CUP, MONEY
-
-    latte_coffee = 20
-    latte_water = 350
-    latte_milk = 75
-    latte_money = 7
-
-    requested_cup_num = int(input("How many cups of coffee do you need: "))
-
-    max_cups_water = WATER // latte_water
-    max_cups_milk = MILK // latte_milk
-    max_cups_coffee = COFFEE // latte_coffee
+    max_cups_water = WATER // water
+    max_cups_milk = MILK // milk if milk > 0 else MILK
+    max_cups_coffee = COFFEE // coffee
     max_cups = min(max_cups_water, max_cups_coffee, max_cups_milk, CUP)
 
     if max_cups >= requested_cup_num:
         print("Yes, I can prepare that amount") if max_cups == requested_cup_num else print(
             f"Yes, and {max_cups - requested_cup_num} more cup(s)")
+        print(f"Take your {coffee_type}")
+        WATER = WATER - water * requested_cup_num
+        COFFEE = COFFEE - coffee * requested_cup_num
+        MILK = MILK - milk * requested_cup_num
+        CUP -= requested_cup_num
+        MONEY = MONEY + money * requested_cup_num
     else:
         print(f"No, I can prepare only {max_cups} cup(s)") if max_cups != 0 else print(
             "No, I can not prepare that amount")
-
-    if latte_coffee * requested_cup_num <= COFFEE and latte_water * requested_cup_num <= WATER and latte_milk * requested_cup_num <= MILK:
-
-        WATER -= latte_water
-        COFFEE -= latte_coffee
-        MILK -= latte_milk
-        CUP -= 1
-        MONEY += latte_money
-    else:
-        print("Sorry, but there is not enough ingredients")
-
-
-def buy_cappuccino():
-    global WATER, MILK, COFFEE, CUP, MONEY
-
-    cap_coffee = 12
-    cap_water = 200
-    cap_milk = 100
-    cap_money = 6
-
-    requested_cup_num = int(input("How many cups of coffee do you need: "))
-
-    max_cups_water = WATER // cap_water
-    max_cups_milk = MILK // cap_milk
-    max_cups_coffee = COFFEE // cap_coffee
-    max_cups = min(max_cups_water, max_cups_coffee, max_cups_milk, CUP)
-
-    if max_cups >= requested_cup_num:
-        print("Yes, I can prepare that amount") if max_cups == requested_cup_num else print(
-            f"Yes, and {max_cups - requested_cup_num} more cup(s)")
-    else:
-        print(f"No, I can prepare only {max_cups} cup(s)") if max_cups != 0 else print(
-            "No, I can not prepare that amount")
-
-    if cap_coffee <= COFFEE and cap_water <= WATER and cap_milk <= MILK:
-        print("Take your capuccino")
-        WATER -= cap_water
-        COFFEE -= cap_coffee
-        MILK -= cap_milk
-        CUP -= 1
-        MONEY += cap_money
-    else:
-        print("Sorry, but there is not enough ingredients")
 
 
 def buy():
-    global WATER, MILK, COFFEE, CUP, MONEY
     coffee_choice = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:"))
     if coffee_choice == 1:
-        buy_espresso()
+        buy_coffee(coffee=16, water=250, milk=0, money=4, coffee_type="espresso")
     elif coffee_choice == 2:
-        buy_latte()
+        buy_coffee(coffee=20, water=350, milk=75, money=7, coffee_type="latte")
     elif coffee_choice == 3:
-        buy_cappuccino()
+        buy_coffee(coffee=12, water=200, milk=100, money=6, coffee_type="cappuccino")
 
 
 def main():
-    global WATER, MILK, COFFEE, CUP, MONEY
+    """
+    Runs a coffee machine program
+    :return: None
+    """
     while True:
-
-        choice = input("Write action (buy, fill, take)")
+        choice = input("Write action (buy, fill, take, exit): ")
 
         if choice == "buy":
             buy()
